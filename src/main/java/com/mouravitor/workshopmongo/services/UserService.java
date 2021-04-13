8e7c9d78ce2e1d852df0726d143905137619e82c
Vitor Moura <moura.vitor20@hotmail.com>
2021-04-12 22:16:29 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/mouravitor/workshopmongo/services/UserService.java
@@ -0,0 +1,23 @@
+package com.mouravitor.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mouravitor.workshopmongo.domain.User;
+import com.mouravitor.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+		
+		
+	}
+
+}
